850b4214708b7c354cdc63bca6cf0e41a5dd10bf
Korspy <vkiim404@gmail.com>
2024-02-16 19:04:44 +1000

    Пракитически всё

--- bot.py
+++ bot.py
@@ -1,22 +1,25 @@
 from aiogram import Bot, types
 from aiogram.types import ReplyKeyboardMarkup
-from aiogram.dispatcher import Dispatcher
+from aiogram.dispatcher import dispatcher
 from aiogram.utils import executor
+from aiogram.types import input_media
+from aiogram.types import input_file
+
+import asyncio
 import requests
 import io
 import os
 from PIL import Image
 from typing import List
 
+from aiogram import __version__
 
 from config import TOKEN, admins_id
 
-
-URI_info = f"https://api.telegram.org/bot{TOKEN}/getFile?file_id="
-URI = f"https://api.telegram.org/file/bot{TOKEN}/" 
-
+print(__version__)
 bot = Bot(token=TOKEN)
-dp = Dispatcher(bot)
+dp = dispatcher.Dispatcher(bot)
+
 
 main = ReplyKeyboardMarkup(resize_keyboard=True)
 main.add("Химчистка обуви").add("Химчистка сумок").add("Чистка аксессуаров из кожи")
@@ -32,7 +35,7 @@ master_panel.add('Просмотр заказов')
 async def process_start_command(message: types.Message):
     await message.reply(f"Здравствуйте, {message.from_user.first_name}! Я бот-менеджер, призванный упростить ваше общение и сэкономить время. \nКакая услуга вас интересует?", reply_markup=main)
     if message.from_user.id == int(admins_id):
-        await message.answer("Вы находитесь в мастер режиме", reply_markup=main_master) 
+        await message.reply("Вы находитесь в мастер режиме", reply_markup=main_master) 
 
 
 @dp.message_handler(commands=['help']) 
@@ -42,44 +45,42 @@ async def process_help_command(message: types.Message):
 
 @dp.message_handler(text="Химчистка обуви")
 async def cleaning(message: types.Message):
-    await message.answer('Отправьте фото обуви', reply_markup=types.ReplyKeyboardRemove())
+    await message.reply('Отправьте фото обуви', reply_markup=types.ReplyKeyboardRemove())
 
 
 @dp.message_handler(text="Химчистка сумок")
 async def cleaning(message: types.Message):
-    await message.answer("Отправьте фото сумки", reply_markup=types.ReplyKeyboardRemove())
+    await message.reply("Отправьте фото сумки", reply_markup=types.ReplyKeyboardRemove())
 
 
 @dp.message_handler(text="Чистка аксессуаров из кожи")
 async def cleaning(message: types.Message):
-    await message.answer("Отправьте фото изделия", reply_markup=types.ReplyKeyboardRemove())
+    await message.reply("Отправьте фото изделия", reply_markup=types.ReplyKeyboardRemove())
 
 
 @dp.message_handler(text="Мастер панель")
 async def admin(message: types.Message):
-    await message.answer("Вы вошли в мастер режим", reply_markup=master_panel)
-
-
-@dp.message_handler()
-async def save_user_response(message: types.Message, messagee_id = ''):
-    price = message.text
-    # path_price = 'database/' + lil + '/price.txt'
-    # with open(path_price, 'r+') as file:
-    #     file.write(price)
-    await bot.send_message(chat_id=messagee_id, text=price)
+    await message.reply("Вы вошли в мастер режим", reply_markup=master_panel)
 
 
 @dp.message_handler(text="Просмотр заказов")
 async def watching_orders(message: types.Message):
     for i in range(len(os.listdir("database"))):
         if len(os.listdir('database' + '/' + os.listdir(f"database")[i])) != 0:
-            phhot = open('database' + '/' + os.listdir('database')[i] + '/' + os.listdir('database' + '/' + os.listdir('database')[i])[0], 'rb')
-            await bot.send_photo(chat_id=message.chat.id, photo=phhot)
-            await message.answer("Оцените стоимость данной работы")
-            print(type(message.chat.id))
-            # await save_user_response(messagee_id = os.listdir('database')[i])
-            break
-    
+            a = input_file.InputFile('database' + '/' + os.listdir('database')[i] + '/' + os.listdir('database' + '/' + os.listdir('database')[i])[0])
+            phhot = input_media.InputMediaPhoto(a)
+            await bot.send_photo(chat_id=message.from_user.id,photo=a)
+            await message.reply("Оцените стоимость данной работы, а затем она будет отправлена", reply_markup=master_panel)
+            path_price = 'database/' + os.listdir('database')[i]
+            with open(path_price, 'r+') as file:
+                file.write(save_user_response())
+
+
+@dp.message_handler()
+async def save_user_response(message: types.Message):
+    price = message.text
+    return price
+
 
 @dp.message_handler(content_types=['photo'])
 async def save_photo(message: types.Message):
@@ -88,16 +89,16 @@ async def save_photo(message: types.Message):
     file = await bot.get_file(file_id)
     photo_path = file.file_path
     downloaded_photo = await bot.download_file(photo_path)
-    if not os.path.exists(f"database/{message.chat.id}"):
-        os.mkdir(f"database/{message.chat.id}")
-    with open(f"database/{message.chat.id}/{message.message_id}.jpg", 'wb') as new_photo_file:
+    if not os.path.exists(f"database/{message.from_user.id}"):
+        os.mkdir(f"database/{message.from_user.id}")
+    with open(f"database/{message.from_user.id}/{message.message_id}.jpg", 'wb') as new_photo_file:
         new_photo_file.write(downloaded_photo.read())
-    await message.answer("Фото будет переслано мастеру, мы ответим вам как только мастер оценит стоимость работы", reply_markup=main)
+    await message.reply("Фото будет переслано мастеру, мы ответим вам как только мастер оценит стоимость работы", reply_markup=main)
 
 
 @dp.message_handler()
 async def fignya(message: types.Message):
-    await message.answer("Я устал /help")
+    await message.reply("Я устал /help")
 
 # тык
     
